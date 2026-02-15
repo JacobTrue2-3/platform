da3cc43c3a723e65d0279498704ebac41d594071
JacobTrue <trukhanyakov@gmail.com>
2026-02-15 11:58:01 +0700

    Теперь пользователи при создании поста могут добавить для неё картинку Отключил html-валидацию для post_form

--- blog/forms.py
+++ blog/forms.py
@@ -6,15 +6,17 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'text']
+        fields = ['title', 'text', 'image']
         widgets = {
             'title': forms.TextInput(attrs={
                 'placeholder': "заголовок (максимальная длина 200 символов)"
-            })
+            }),
+            'image': forms.FileInput(attrs={'class': 'form-control'})
         }
         labels = {
             'title': 'Заголовок поста:',
-            'text': 'Текст поста:'
+            'text': 'Текст поста:',
+            'image': 'Картинка поста'
         }
 
     def clean_title(self):
--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ User = get_user_model()
 class Post(models.Model):
     title = models.CharField(max_length=200, verbose_name="Заголовок")
     text = models.TextField(verbose_name="Текст")
-    image = models.ImageField(upload_to="post_images/", null=True)
+    image = models.ImageField(upload_to="post_images/", null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts') # можно указать SET_NULL
 
