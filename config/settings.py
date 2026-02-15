cba7eae74509254b0e58a9df2f22c49741dc0543
JacobTrue <trukhanyakov@gmail.com>
2026-02-15 11:58:16 +0700

    Деплой (2) - Debug=False, Nginx и Gunicorn. DB - POSTGES.

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '77.232.139.86']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '77.232.139.86', 'insiders-crys.ru', 'www.insiders-crys.ru']
 
 
 # Application definition
