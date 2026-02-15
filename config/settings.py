7f3bedf745394a8ca80b8dbb71e8188885555565
JacobTrue <trukhanyakov@gmail.com>
2026-02-15 11:58:16 +0700

    Деплой (1) - Debug=False, Nginx и Gunicorn.

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '77.232.139.86']
 
@@ -124,12 +124,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static/']
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / 'static/']
+else:
+    STATIC_ROOT = '/home/python521user/Pikabu-E-python521/static'
 
 
 # Media Files (User files)
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media/'
+if DEBUG:
+    MEDIA_ROOT = BASE_DIR / 'media/'
+else:
+    MEDIA_ROOT = '/home/python521user/Pikabu-E-python521/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
