f575abfa57bd03110f0269ce21f2d758f715cb58
JacobTrue <trukhanyakov@gmail.com>
2026-02-15 11:58:04 +0700

    Переписал logout_view на CBV

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import logout, get_user_model
 from django.views.generic import CreateView
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 
 from config.settings import LOGIN_REDIRECT_URL
@@ -28,9 +28,8 @@ class CustomLoginView(LoginView):
         return next_url
 
 
-def logout_view(request):
-    logout(request)
-    return redirect("blog:post_list")
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy('blog:post_list')
 
 
 def profile_view(request, user_username):
