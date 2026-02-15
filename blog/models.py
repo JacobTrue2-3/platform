dfebf79b47f62121f2751c0c8dda9fd928279fec
JacobTrue <trukhanyakov@gmail.com>
2026-02-15 11:58:00 +0700

    Добавил поле author в модель Post

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Post(models.Model):
     title = models.CharField(max_length=200, verbose_name="Заголовок")
     text = models.TextField(verbose_name="Текст")
     created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts') # можно указать SET_NULL
 
     class Meta:
         verbose_name = 'Пост'
@@ -12,3 +16,5 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+# null, default
