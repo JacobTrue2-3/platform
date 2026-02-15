4af0e94657d39c90b8ea03c100ee9bab6222371e
JacobTrue <trukhanyakov@gmail.com>
2026-02-15 11:58:12 +0700

    Добавил функционал сброса пароля - "Забыли пароль?". Добавил настройки email (smtp).

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-# from django.urls import reverse_lazy
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 # from django.contrib.auth.views import PasswordChangeView
 from . import views
 
@@ -16,6 +17,29 @@ urlpatterns = [
     #     success_url=reverse_lazy('users:password_change_done')
     # ), name='password_change'),
     path('password-change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name="users/pages/password_reset.html",
+        email_template_name='users/emails/password_reset.txt',
+        html_email_template_name='users/emails/password_reset.html',
+        subject_template_name='users/emails/subjects/password_reset.txt',
+        success_url=reverse_lazy("users:password_reset_instructions_sent")
+    ), name='password_reset'),
+
+    path('password-reset/instructions-sent/', auth_views.PasswordResetDoneView.as_view(
+        template_name="users/pages/password_reset_instructions_sent.html"
+    ), name='password_reset_instructions_sent'),
+
+    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name="users/pages/password_reset_form.html",
+        success_url=reverse_lazy('users:password_reset_complete')
+    ), name='password_reset_set_new'),
+
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name="users/pages/password_reset_complete.html"
+    ), name='password_reset_complete'),
+
 
     path("toggle-theme/", views.toggle_theme, name="toggle_theme"),
 
