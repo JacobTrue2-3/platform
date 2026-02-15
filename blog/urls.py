db4a242dc83057a98085179e4c9253e1dad3245f
JacobTrue <trukhanyakov@gmail.com>
2026-02-15 11:57:58 +0700

    Добавил функционал для удаления поста

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import get_post_list, get_post_detail, create_post, update_post
+from .views import get_post_list, get_post_detail, create_post, update_post, delete_post
 
 urlpatterns = [
     path("posts/", get_post_list, name="post_list"),
     path('posts/<int:post_id>/', get_post_detail, name="post_detail"),
     path('posts/add/', create_post, name="new_post"),
-    path('posts/<int:post_id>/edit/', update_post, name="edit_post")
+    path('posts/<int:post_id>/edit/', update_post, name="edit_post"),
+    path('posts/<int:post_id>/delete/', delete_post, name="remove_post")
 ]
