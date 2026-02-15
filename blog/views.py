ec99e779bf64d1e754e0ec876a3e790a6d82242f
JacobTrue <trukhanyakov@gmail.com>
2026-02-15 11:57:58 +0700

    Создал универсальный шаблон для создания и удаления поста

--- blog/views.py
+++ blog/views.py
@@ -20,10 +20,13 @@ def get_post_detail(request, post_id):
 
 
 def create_post(request):
+    title = "Создать пост"
+    submit_button_text = 'Создать'
+
     if request.method == "GET":
         form = PostForm()
 
-        return render(request, 'blog/post_add.html', context={"form": form})
+        return render(request, 'blog/post_form.html', context={"form": form, 'title': title, 'submit_button_text': submit_button_text})
     
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -34,10 +37,13 @@ def create_post(request):
 
             return redirect('post_detail', post_id=post.id)
         else:
-            return render(request, 'blog/post_add.html', context={"form": form})
+            return render(request, 'blog/post_form.html', context={"form": form, 'title': title, 'submit_button_text': submit_button_text})
 
 
 def update_post(request, post_id):
+    title = "Редактировать пост"
+    submit_button_text = 'Обновить'
+
     post = get_object_or_404(Post, id=post_id)
 
     if request.method == "POST":
@@ -48,8 +54,8 @@ def update_post(request, post_id):
 
             return redirect("post_detail", post_id=updated_post.id)
         else:
-            return render(request, 'blog/post_update.html', context={"form": form})
+            return render(request, 'blog/post_form.html', context={"form": form, 'title': title, 'submit_button_text': submit_button_text})
 
     form = PostForm(instance=post)
 
-    return render(request, 'blog/post_update.html', context={"form": form})
+    return render(request, 'blog/post_form.html', context={"form": form, 'title': title, 'submit_button_text': submit_button_text})
