61a6c10c8b925521f0bcb70feb671d531f1e2fb7
JacobTrue <trukhanyakov@gmail.com>
2026-02-15 11:57:59 +0700

    Реализовал функционал входа в аккаунт

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 
 def register_view(request):
@@ -7,11 +8,27 @@ def register_view(request):
         form = UserCreationForm(request.POST)
 
         if form.is_valid():
-            form.save()
-            return redirect('blog:post_list')
+            return redirect('users:login')
         else:
             return render(request, 'users/register.html', {'form': form})
 
     form = UserCreationForm()
 
     return render(request, 'users/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+
+            return redirect("blog:post_list")
+        else:
+            return render(request, 'users/login.html', {'form': form})
+
+
+    form = AuthenticationForm()
+
+    return render(request, 'users/login.html', {'form': form})
