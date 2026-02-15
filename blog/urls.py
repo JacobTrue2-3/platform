3f888d7b6677ac6d3b7ecc94808f6f8ef46723aa
JacobTrue <trukhanyakov@gmail.com>
2026-02-15 11:58:10 +0700

    Добавил отображение количества комментариев к посту и улучшил структуру футера карточки поста

--- blog/urls.py
+++ blog/urls.py
@@ -1,26 +1,22 @@
 from django.urls import path
-from .views import (
-    MainPageView, PostListView, LoadMorePostsView, PostSearchView, CategoryPostsView, TagPostsView,
-    PostDetailView, CreatePostView, PostUpdateView, PostDeleteView,
-    PostLikeToggleView, PostDislikeToggleView, PostFavoriteToggleView, AddCommentView, LoadMoreCommentsView
-)
+from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
-    path("posts/", PostListView.as_view(), name="post_list"),
-    path("posts/load-more/", LoadMorePostsView.as_view(), name="load_more_posts"),
-    path("posts/search/", PostSearchView.as_view(), name="post_search"),
-    path('posts/add/', CreatePostView.as_view(), name="new_post"),
-    path('posts/<int:post_id>/edit/', PostUpdateView.as_view(), name="edit_post"),
-    path('posts/<int:post_id>/delete/', PostDeleteView.as_view(), name="remove_post"),
-    path('posts/<slug:post_slug>/', PostDetailView.as_view(), name="post_detail"),
-    path('posts/category/<slug:category_slug>/', CategoryPostsView.as_view(), name="category_posts"),
-    path('posts/tag/<slug:tag_slug>/', TagPostsView.as_view(), name="tag_posts"),
-    path('posts/<int:post_id>/like/', PostLikeToggleView.as_view(), name="post_like"),
-    path('posts/<int:post_id>/dislike/', PostDislikeToggleView.as_view(), name="post_dislike"),
-    path('posts/<int:post_id>/toggle-favorite/', PostFavoriteToggleView.as_view(), name="post_favorite_toggle"),
-    path("posts/<int:post_id>/comments/add/", AddCommentView.as_view(), name="add_comment"),
-    path('posts/<int:post_id>/comments/load-more/', LoadMoreCommentsView.as_view(), name="load_more_comments"),
-    path('', MainPageView.as_view(), name='main_page'),
+    path("posts/", views.PostListView.as_view(), name="post_list"),
+    path("posts/load-more/", views.LoadMorePostsView.as_view(), name="load_more_posts"),
+    path("posts/search/", views.PostSearchView.as_view(), name="post_search"),
+    path('posts/add/', views.CreatePostView.as_view(), name="new_post"),
+    path('posts/<int:post_id>/edit/', views.PostUpdateView.as_view(), name="edit_post"),
+    path('posts/<int:post_id>/delete/', views.PostDeleteView.as_view(), name="remove_post"),
+    path('posts/<slug:post_slug>/', views.PostDetailView.as_view(), name="post_detail"),
+    path('posts/category/<slug:category_slug>/', views.CategoryPostsView.as_view(), name="category_posts"),
+    path('posts/tag/<slug:tag_slug>/', views.TagPostsView.as_view(), name="tag_posts"),
+    path('posts/<int:post_id>/like/', views.PostLikeToggleView.as_view(), name="post_like"),
+    path('posts/<int:post_id>/dislike/', views.PostDislikeToggleView.as_view(), name="post_dislike"),
+    path('posts/<int:post_id>/toggle-favorite/', views.PostFavoriteToggleView.as_view(), name="post_favorite_toggle"),
+    path("posts/<int:post_id>/comments/add/", views.AddCommentView.as_view(), name="add_comment"),
+    path('posts/<int:post_id>/comments/load-more/', views.LoadMoreCommentsView.as_view(), name="load_more_comments"),
+    path('', views.MainPageView.as_view(), name='main_page'),
 ]
