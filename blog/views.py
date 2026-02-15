8fd844ddbb8313f69b650a3c368df7a8b5f2a23c
JacobTrue <trukhanyakov@gmail.com>
2026-02-15 11:58:06 +0700

    Добавил пагинацию на страницу post_search (учёл GET-параметры)

--- blog/views.py
+++ blog/views.py
@@ -20,6 +20,7 @@ class PostSearchView(ListView):
     model = Post
     template_name = "blog/post_search.html"
     context_object_name = 'posts'
+    paginate_by = 3
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
